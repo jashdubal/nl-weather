cc8334c755b829dd624cf8684be567a50e851fbb
jash dubal <73313777+jashdubal@users.noreply.github.com>
2022-09-24 11:00:13 -0600

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,9 +1,10 @@
 # import the module
+from xmlrpc.client import Boolean, boolean
 import python_weather
 import asyncio
 import os
 
-async def getweather():
+async def getweather(self):
   # declare the client. format defaults to the metric system (celcius, km/h, etc.)
   async with python_weather.Client(format=python_weather.METRIC) as client:
 
@@ -11,16 +12,24 @@ async def getweather():
     weather = await client.get("Calgary, Alberta")
 
     # returns the current day's forecast temperature (int)
-    print(weather.current.temperature)
-    print(weather.current.description)
-    print(weather.current.humidity)
-    print(weather.current.feels_like)
+    self.temp_is_negative = False
+    self.temp_first = weather.current.temperature // 10
+    self.temp_second = weather.current.temperature % 10
+    if(weather.current.temperature < 0):
+      self.temp_is_negative = True
+    
+    weather_description = weather.current.description
+    weather_humidity = weather.current.humidity
+    weather_feel_like = weather.current.feels_like
 
-if __name__ == "__main__":
-  # see https://stackoverflow.com/questions/45600579/asyncio-event-loop-is-closed-when-getting-loop
-  # for more details
 
-  if os.name == "nt":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+class Weather:
+  temp_first: int
+  temp_second: int
+  temp_is_negative: Boolean
 
-  asyncio.run(getweather())
+  def __init__(self):
+    if os.name == "nt":
+      asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    asyncio.run(getweather(self))
